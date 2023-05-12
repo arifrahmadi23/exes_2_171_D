ef90331b7506a5e84fa26543bac9402e3a2f2671
arifrahmadi23 <muhammad231arif@gmail.com>
2023-05-12 09:15:17 +0700

    Add project files.

---
+++ exes_2_171_D/exes_2_171_D.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int arif[68];
+int n;
+int i;
+
+void input()
+{
+	while (true)
+	{
+		cout << "Masukan Jumlah Panjang Data : ";
+		cin >> n;
+		if ((n > 0) && (n <= 68))
+			break;
+		else << "\nPanjang Data Minimum 1 dan Maximum 68 Elemennt.\n\n";
+	}
+
+	cout << "\n-------------------\n";
+	cout << "Masukan Data\n";
+	cout <<
+}
