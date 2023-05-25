602f6226a4147e87090da343f01d7327529e3be1
arifrahmadi23 <muhammad231arif@gmail.com>
2023-05-25 11:31:40 +0700

    baru commit

--- exes_2_171_D/exes_2_171_D.cpp
+++ exes_2_171_D/exes_2_171_D.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int arif[68];
+int arif[31];
 int n;
 int i;
 
@@ -9,14 +9,73 @@ void input()
 {
 	while (true)
 	{
-		cout << "Masukan Jumlah Panjang Data : ";
+		cout << "Masukan Panjang Data : ";
 		cin >> n;
-		if ((n > 0) && (n <= 68))
+		if ((n > 0) && (n <= 31))
 			break;
-		else << "\nPanjang Data Minimum 1 dan Maximum 68 Elemennt.\n\n";
+		else 
+			cout << "\nPanjang Data Minimum 1 dan Maximum 31 Data.\n\n";
 	}
 
-	cout << "\n-------------------\n";
+	cout << "\n-------------\n";
 	cout << "Masukan Data\n";
-	cout <<
+	cout << "-------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "Data ke <" << (i + 1) << "> ";
+		cin >> arif[i];
+	}
 }
+
+void BinarySearch()
+{
+	int muhammad;
+	int rahmadi;
+	int item;
+	int mid;
+	char c;
+
+	do
+	{
+		cout << "Masukan Data yang ingin dicari : ";
+		cin >> item;
+
+		muhammad = 0;
+		rahmadi = n - 1;
+		mid = (muhammad + rahmadi) / 2;
+
+		while (muhammad <= rahmadi)
+		{
+			if (arif[mid] == item)
+			{
+				cout << "\n" <<item  << " Posisinya di data ke- " << (mid + 1) << endl;
+				break;
+			}
+			else if (item < arif[mid])
+			{
+				rahmadi = mid - 1;
+			}
+			else if (item > arif[mid])
+			{
+				muhammad = mid + 1;
+			}
+			mid = (muhammad + rahmadi) / 2;
+
+		}
+		if (muhammad > rahmadi)
+		
+			cout << "\nData tidak ditemukan\n";
+
+			cout << "\nLanjut cari? (y/n): ";
+			cin >> c;
+		
+	} while ((c == 'y') || (c == 'Y'));
+}
+
+int main()
+{
+	input();
+	BinarySearch();
+	return 0;
+}
+
